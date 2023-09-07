97c4cdba2e5fc54a9761a03405a6293e1c75fff6
ehvkddl <ehvkddl@naver.com>
2023-09-08 05:02:50 +0900

    add: BaseCollectionViewCell 추가

---
+++ SeSACShopping/SeSACShopping/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseCollectionViewCell.swift
+//  SeSACShopping
+//
+//  Created by do hee kim on 2023/09/08.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell() { }
+    
+    func setConstraints() { }
+    
+}
