04abfd92cc10f00e03d02e30b72b1db317355c53
ehvkddl <ehvkddl@naver.com>
2023-09-07 16:28:13 +0900

    init: SeSACShopping 생성

---
+++ SeSACShopping/SeSACShopping/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACShopping
+//
+//  Created by do hee kim on 2023/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
