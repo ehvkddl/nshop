4ef288c88613caaeb5a8fd14828774bf10431e5d
ehvkddl <ehvkddl@naver.com>
2024-03-23 18:34:08 +0900

    feat: 상품 검색시 debounce 추가 #1

--- SeSACShopping/SeSACShopping/Network/Debouncer.swift
+++ SeSACShopping/SeSACShopping/Network/Debouncer.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class Debouncer {
+    
+    private var workItem: DispatchWorkItem?
+    private let seconds: Int
+    
+    init(seconds: Int) {
+        self.seconds = seconds
+    }
+    
+    func run(_ closure: @escaping () -> ()) {
+        self.workItem?.cancel()
+        
+        let newWork = DispatchWorkItem(block: closure)
+        workItem = newWork
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(seconds), execute: newWork)
+    }
+    
+}
